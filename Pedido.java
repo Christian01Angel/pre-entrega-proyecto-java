4f03bb8f200802a6ced811097a8f25f32cc4ba72
Christian Angel <christianangel94@gmail.com>
2025-05-19 23:08:28 -0300

    Creando la clase pedido

--- Pedido.java
+++ Pedido.java
@@ -2,20 +2,30 @@ package poo;
 import java.util.ArrayList;
 
 public class Pedido extends Articulo {
-	private static int id;
+	private static int id = 0;
 	private double total;
 	private int cantidad;
+	private ArrayList<Articulo> pedido;
 	
 	public Pedido(String nombre, double precio, int cantidad) {
 		super(nombre, precio);
 		this.cantidad = cantidad;
 	}
 	
-	
-	
-	public double calcularTotal() {
-		System.out.println("Hola");
-		double num = 1.5;
-		return num;
+	public void agregarProducto(Articulo producto) {
+		//La validacion se hace en el main, al agregar un producto por su ID. Aca se agrega el producto completo + la cantidad a pedir de ese producto.
+		pedido.add(producto);
 	}
-}
+	
+	public void finalizarPedido() {
+		//Una vez finalizado el pedido incrementamos el id e imprimimos el pedido.
+		id+=1;
+	}
+	
+	public double calcularTotal(ArrayList<Articulo> pedidos) {
+		total = 0;
+		for (Articulo articulo: pedidos) {
+		}
+		return this.total;
+	}
+}	
